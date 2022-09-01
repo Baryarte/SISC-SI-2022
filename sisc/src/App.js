ba6a6d9cb544babbc0533e9ae4a0c7a1fd4749da
GabrielCMoretto <gabrielmorettopvh@yahoo.com.br>
2022-08-31 23:18:01 -0300

    Aprender react router e coloquei uma segunda pagina

--- sisc/src/App.js
+++ sisc/src/App.js
@@ -1,18 +1,43 @@
 ﻿import React from "react";
 import "./App.css";
+import Recomendation from "./components/Recomendation";
 import Searchbar from './components/Searchbar';
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom"
 
 const App = () => {
+  return (
+    <>
 
-    return (
-        <div className="app">
+      <Routes>
+        <Route path="/" element={
+          <div className="app">
             <div className="container">
-                <h1 className="page-title">SISTEMA DE INFORMAÇÃO SOBRE RECICLAGEM</h1>
-                <Searchbar/>
-
+              <h1 className="page-title">SISTEMA DE INFORMAÇÃO SOBRE RECICLAGEM</h1>
+              <h1>Materiais</h1>
+              <Searchbar />
             </div>
-        </div>
-    );
+          </div>} />
+        <Route path="/Locais" element={<div className="app">
+          <div className="container">
+            <h1 className="page-title">SISTEMA DE INFORMAÇÃO SOBRE RECICLAGEM</h1>
+            <h1>Locais</h1>
+            <Searchbar />
+          </div>
+        </div>} />
+        <Route path="/Favoritos" element={<h1>Favoritos</h1>} />
+      </Routes>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/Locais">Locais</Link></li>
+          <li><Link to="/Favoritos">Favoritos</Link></li>
+        </ul>
+      </nav>
+    </>
+
+  );
 };
 
 export default App;
+
+
---
+++ sisc/src/components/Recomendation.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import "./Recomendation.css"
+const Recomendation = () => {
+  return (
+    <h1 className='recomendation'>recomendação</h1>
+  )
+}
+export default Recomendation;
