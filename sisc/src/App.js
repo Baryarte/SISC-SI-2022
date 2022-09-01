278439d91bcbc3ccb019037398110941d86d0b7b
Gabriel Carvalho Moretto <gabrielmorettopvh@hotmail.com.br>
2022-09-01 02:06:11 -0300

    Consegui colocar umas imagens

--- sisc/src/App.js
+++ sisc/src/App.js
@@ -2,7 +2,10 @@
 import "./App.css";
 import Recomendation from "./components/Recomendation";
 import Searchbar from './components/Searchbar';
-import { BrowserRouter, Route, Routes, Link } from "react-router-dom"
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import Img_Papel from './images/papel-reciclado.jpg';
+import Img_Garrafa from './images/vaso-garrafa-pet.jpg';
+import Img_Pneu from './images/mesa-pneu.jpg';
 
 const App = () => {
   return (
@@ -16,13 +19,11 @@ const App = () => {
               <h1>Materiais</h1>
               <Searchbar />
               <h1>Mais pesquisados:</h1>
-              <div className="reccontainer">
-                <nav>
-                  <ul>
-                    <li><Link to="/1">1</Link></li>
-                    <li><Link to="/2">2</Link></li>
-                    <li><Link to="/3">3</Link></li>
-                  </ul>
+              <div >
+                <nav className="reccontainer">
+                  <img className="rec" src={Img_Papel} alt="papel" />
+                  <img className="rec" src={Img_Garrafa} alt="garrafa" />
+                  <img className="rec" src={Img_Pneu} alt="pneu" />
                 </nav>
               </div>
 
@@ -46,11 +47,11 @@ const App = () => {
         <Route path="/3" element={<h1>3</h1>} />
       </Routes>
       <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/Locais">Locais</Link></li>
-          <li><Link to="/Favoritos">Favoritos</Link></li>
-        </ul>
+
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/Locais">Locais</Link></li>
+        <li><Link to="/Favoritos">Favoritos</Link></li>
+
       </nav>
     </>
 
