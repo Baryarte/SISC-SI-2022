04178a2cfe4ffa76ecd174529abfaff21b46d270
Thiaggo Ferreira <thiaggo98@gmail.com>
2022-09-05 22:50:17 -0300

    Comentários e avanço a página da receita

--- sisc/src/firebase/queries.js
+++ sisc/src/firebase/queries.js
@@ -1,54 +1,61 @@
-import {addDoc, collection, getDoc, getDocs, where, query, doc} from "firebase/firestore";
-import {db} from "./firebase-config";
+import {
+  addDoc,
+  collection,
+  getDoc,
+  getDocs,
+  where,
+  query,
+  doc,
+} from "firebase/firestore";
+import { db } from "./firebase-config";
 
-const testFirebase = async () => {
-    try {
-        const docRef = await addDoc(collection(db, "tests"), {
-            name: "test",
-        });
-        console.log("Document written with ID: ", docRef.id);
-    } catch (e) {
-        console.error("Error adding document: ", e);
-    }
-}
+// const testFirebase = async () => {
+//     try {
+//         const docRef = await addDoc(collection(db, "tests"), {
+//             name: "test",
+//         });
+//         console.log("Document written with ID: ", docRef.id);
+//     } catch (e) {
+//         console.error("Error adding document: ", e);
+//     }
+// }
 
-const getMaterials = async ( material ) => {
+// Pega uma array de materiais pelo tipo do material
+const getMaterials = async (material) => {
+  const materialsRef = collection(db, "materials");
 
-    const materialsRef = collection(db, "materials");
+  const q = query(materialsRef, where("type", "==", material));
 
-    const q = query(materialsRef, where("type", "==", material));
+  const querySnapshot = await getDocs(q);
 
-    const querySnapshot = await getDocs(q);
+  console.log("querySnapshot");
+  querySnapshot.forEach((doc) => console.log(doc.id, " => ", doc.data()));
 
-    console.log("querySnapshot");
-    querySnapshot.forEach(doc => console.log(doc.id, " => ", doc.data()));
+  return querySnapshot;
+};
 
-    return querySnapshot;
+// Pega um material pelo Id do material
+const getMaterial = async (materialId) => {
+  const materialRef = doc(db, "materials", materialId);
 
-}
+  const material = await getDoc(materialRef);
 
-const getMaterial = async ( materialId ) => {
+  console.log("material", material.data(), material.id);
 
-    const materialRef = doc(db, "materials", materialId);
+  return material;
+  // access the document data with the .data() method
+};
 
-    const material = await getDoc(materialRef);
+// Pega um material pelo Id da receita
+const getRecipe = async (recipeId) => {
+  const recipeRef = doc(db, "recipes", recipeId);
 
-    console.log("material", material.data(), material.id);
+  const recipe = await getDoc(recipeRef);
 
-    return material;
-    // access the document data with the .data() method
-}
+  console.log("recipe", recipe.data(), recipe.id);
 
-const getRecipe = async ( recipeId ) => {
+  return recipe;
+  // access the document data with the .data() method
+};
 
-        const recipeRef = doc(db, "recipes", recipeId);
-
-        const recipe = await getDoc(recipeRef);
-
-        console.log("recipe", recipe.data(), recipe.id);
-
-        return recipe;
-        // access the document data with the .data() method
-}
-
-export {testFirebase, getMaterial,  getMaterials, getRecipe};
+export { getMaterial, getMaterials, getRecipe };
--- sisc/src/pages/Recipe/Recipe.js
+++ sisc/src/pages/Recipe/Recipe.js
@@ -1,23 +1,35 @@
-import React, {useEffect, useState} from 'react';
-import {useLocation, useParams} from "react-router-dom";
-import {getRecipe} from "../../firebase/queries";
+import React, { useEffect, useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import { getRecipe } from "../../firebase/queries";
 
 function Recipe(props) {
-    const [recipe, setRecipe] = useState();
-    const {recipeId} = useLocation().state;
-    console.log(props)
-    console.log("recipeId", recipeId)
-    useEffect(() => {
-        (async () => {
-            const recipeData = await getRecipe(recipeId);
-            setRecipe(recipeData);
-            console.count("recipeData");
-        })();
-    }, [])
+  const [recipe, setRecipe] = useState();
+  const { recipeId } = useLocation().state;
 
-    return (
-        <h1></h1>
-    );
+  useEffect(() => {
+    (async () => {
+      const recipeData = await getRecipe(recipeId);
+      if (recipeData.exists()) {
+        setRecipe(recipeData.data());
+      }
+      console.count("recipeData");
+    })();
+  }, []);
+
+  useEffect(() => {
+    console.log(recipe && recipe.name);
+  }, [recipe]);
+
+  const steps =
+    recipe && recipe.steps.split(".").map((step) => <li>{step.trim()}</li>);
+
+  return (
+    <div>
+      <h1>{recipe && recipe.name}</h1>
+      <ol>{steps}</ol>
+      <p>aaaa</p>
+    </div>
+  );
 }
 
 export default Recipe;
