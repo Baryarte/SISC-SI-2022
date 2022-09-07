5e13821d09a1a4831437b8a85b38fd02d5b4f63c
Thiaggo Ferreira <thiaggo98@gmail.com>
2022-09-07 17:43:57 -0300

    Revert "Revert "Página de receita finalizada""
    This reverts commit 6cec74c400206deedf21213b1b9edfa6c7dc39d1.

--- sisc/src/pages/Recipe/Recipe.js
+++ sisc/src/pages/Recipe/Recipe.js
@@ -5,7 +5,9 @@ import "./Recipe.css";
 
 function Recipe(props) {
     const [recipe, setRecipe] = useState();
+    const [favorite, setFavorite] = useState();
     const {recipeId} = useLocation().state;
+
 
     useEffect(() => {
         (async () => {
@@ -26,30 +28,42 @@ function Recipe(props) {
     const materials = recipe && recipe.materials.map(material => <li>{material}</li>);
 
     return (
-        <div>
-            <h1 className={"recipe-title"}>{recipe && recipe.name}</h1>
-            <span className={"star"}>&#9733;</span>
-            <figure>
-                <img className={"recipe-image"} src={recipe && recipe.image}/>
-                <figcaption>Papel reciclado feito em casa</figcaption>
-            </figure>
-            <div>
-                <h2 className={"recipe-ingredients-header"}>Ingredientes:</h2>
-                <ul className={"recipe-ingredients-list"}>
-                    {ingredients}
-                </ul>
+        <div className={"recipe-page-container"}>
+            <div className={"recipe-title-container"}>
+                <h1 className={"recipe-title"}>{recipe && recipe.name}</h1>
+                <span className={favorite ? "star on" : "star off"}>&#9733;</span>
             </div>
-            <div>
-                <h2 className={"recipe-materials-header"}>Materiais:</h2>
-                <ul className={"recipe-materials-list"}>
-                    {materials}
-                </ul>
+            <div className={"recipe-first-row"}>
+
+                <figure className={"recipe-figure"}>
+                    <img className={"recipe-image"} src={recipe && recipe.image}/>
+                    <figcaption className={"recipe-figure-caption"}>Papel reciclado feito em casa</figcaption>
+                </figure>
+                <div>
+                    <div>
+                        <h2 className={"recipe-ingredients-header"}>Ingredientes:</h2>
+                        <ul className={"recipe-ingredients-list"}>
+                            {ingredients}
+                        </ul>
+                    </div>
+                    <div>
+                        <h2 className={"recipe-materials-header"}>Materiais:</h2>
+                        <ul className={"recipe-materials-list"}>
+                            {materials}
+                        </ul>
+                    </div>
+                </div>
             </div>
-            <div>
+            <div className={"recipe-steps-container"}>
                 <p className={"recipe-steps-header"}>Passo a passo:</p>
                 <ol className={"recipe-steps-list"}>
                     {steps}
                 </ol>
+            </div>
+
+            <div>
+                <p className={"recipe-source-header"}>Fonte:</p>
+                <p className={"recipe-source"}>{recipe && recipe.sources}</p>
 
             </div>
         </div>
