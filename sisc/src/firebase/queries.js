6a0b8450273828deb0d1a4117729d7afc01a2f4c
Thiaggo Ferreira <thiaggo98@gmail.com>
2022-09-07 21:31:47 -0300

    getMaterialRecipes query feita

--- sisc/src/firebase/queries.js
+++ sisc/src/firebase/queries.js
@@ -1,13 +1,13 @@
 import {
-  addDoc,
-  collection,
-  getDoc,
-  getDocs,
-  where,
-  query,
-  doc,
+    addDoc,
+    collection,
+    getDoc,
+    getDocs,
+    where,
+    query,
+    doc,
 } from "firebase/firestore";
-import { db } from "./firebase-config";
+import {db} from "./firebase-config";
 
 // const testFirebase = async () => {
 //     try {
@@ -22,52 +22,71 @@ import { db } from "./firebase-config";
 
 // Pega uma array de materiais pelo tipo do material
 const getMaterials = async (material) => {
-  const materialsRef = collection(db, "materials");
+    const materialsRef = collection(db, "materials");
 
-  const q = query(materialsRef, where("type", "==", material));
+    const q = query(materialsRef, where("type", "==", material));
 
-  const querySnapshot = await getDocs(q);
+    const querySnapshot = await getDocs(q);
 
-  console.log("querySnapshot");
-  querySnapshot.forEach((doc) => console.log(doc.id, " => ", doc.data()));
+    console.log("querySnapshot");
+    querySnapshot.forEach((doc) => console.log(doc.id, " => ", doc.data()));
 
-  return querySnapshot;
+    return querySnapshot;
 };
 
 // Pega um material pelo Id do material
 const getMaterial = async (materialId) => {
-  const materialRef = doc(db, "materials", materialId);
+    const materialRef = doc(db, "materials", materialId);
+    const material = await getDoc(materialRef);
 
-  const material = await getDoc(materialRef);
+    // console.log("material", material.data(), material.id);
 
-  console.log("material", material.data(), material.id);
-
-  return material;
-  // access the document data with the .data() method
+    return material;
+    // access the document data with the .data() method
 };
+
+// Pega as receitas de um material
+const getMaterialRecipes = async (material) => {
+    console.log("material", material.data());
+    const recipes = material.data().recipes;
+    let recipesData = recipes.map(async (recipeObject) => {
+        const recipeName = recipeObject.name;
+        const recipeRef = recipeObject.ref;
+        let recipe = await getDoc(recipeRef);
+        recipe =  recipe.data();
+        return recipe;
+    });
+
+    recipesData = await Promise.all(recipesData);
+
+
+    return recipesData;
+}
 
 // Pega um material pelo Id da receita
 const getRecipe = async (recipeId) => {
-  const recipeRef = doc(db, "recipes", recipeId);
+    const recipeRef = doc(db, "recipes", recipeId);
 
-  const recipe = await getDoc(recipeRef);
+    const recipe = await getDoc(recipeRef);
 
-  console.log("recipe", recipe.data(), recipe.id);
+    // console.log("recipe", recipe.data(), recipe.id);
 
-  return recipe;
-  // access the document data with the .data() method
+    return recipe;
+    // access the document data with the .data() method
 };
+
 
 // Pega um local pelo Id do local
 const getLocal = async (localId) => {
-  const localRef = doc(db, "locals", localId);
+    const localRef = doc(db, "locals", localId);
 
-  const local = await getDoc(localRef);
+    const local = await getDoc(localRef);
 
-  console.log("local", local.data(), local.id);
+    // console.log("local", local.data(), local.id);
 
-  return local;
-  // access the document data with the .data() method
+    return local;
+    // access the document data with the .data() method
 };
 
-export { getMaterial, getMaterials, getRecipe, getLocal };
+
+export {getMaterial, getMaterials, getMaterialRecipes, getRecipe, getLocal};
